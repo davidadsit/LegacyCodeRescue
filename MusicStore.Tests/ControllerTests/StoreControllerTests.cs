0c0ff4201dc0698348724c3a46bb9ab6db9d4990
David Adsit <david.adsit@gmail.com>
2012-07-10 22:33:46 -0600

    Test 2 - StoreController.Details()

--- MusicStore.Tests/ControllerTests/StoreControllerTests.cs
+++ MusicStore.Tests/ControllerTests/StoreControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using MvcMusicStore.Controllers;
 using MvcMusicStore.Models;
 using NUnit.Framework;
@@ -9,13 +10,24 @@ namespace MusicStore.Tests.ControllerTests
 	public class StoreControllerTests
 	{
 		[Test]
+		public void Details_shows_one_album_by_id()
+		{
+			StoreController storeController = new StoreController();
+			MusicStoreEntities storeDb = new MusicStoreEntities();
+
+			ViewResult detailsResult = (ViewResult) storeController.Details(5);
+
+			Assert.AreEqual(storeDb.Albums.Find(5), detailsResult.Model);
+		}
+
+		[Test]
 		public void Index_displays_the_list_of_genres()
 		{
 			//Arrange
 			StoreController storeController = new StoreController();
 			MusicStoreEntities storeDb = new MusicStoreEntities();
 			//Act
-			ViewResult indexResult = (ViewResult)storeController.Index();
+			ViewResult indexResult = (ViewResult) storeController.Index();
 			//Assert
 			Assert.AreEqual(storeDb.Genres, indexResult.Model);
 		}
