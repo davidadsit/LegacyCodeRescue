328e742383268be093d3d8d1989cf356b0bef99f
David Adsit <david.adsit@gmail.com>
2012-07-10 21:40:52 -0600

    Test 1 - StoreController.Index()

---
+++ MusicStore.Tests/ControllerTests/StoreControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using MvcMusicStore.Controllers;
+using MvcMusicStore.Models;
+using NUnit.Framework;
+
+namespace MusicStore.Tests.ControllerTests
+{
+	[TestFixture]
+	public class StoreControllerTests
+	{
+		[Test]
+		public void Index_displays_the_list_of_genres()
+		{
+			//Arrange
+			StoreController storeController = new StoreController();
+			MusicStoreEntities storeDb = new MusicStoreEntities();
+			//Act
+			ViewResult indexResult = (ViewResult)storeController.Index();
+			//Assert
+			Assert.AreEqual(storeDb.Genres, indexResult.Model);
+		}
+	}
+}
