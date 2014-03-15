4ab7891cb67fd51b9fc0858f437ccdcd5cfc01c6
david-adsit <david-adsit@pluralsight.com>
2014-03-14 21:19:01 -0600

    FitNesse cleanup

--- MusicStore.Acceptance/Fixtures/CatalogFixture.cs
+++ MusicStore.Acceptance/Fixtures/CatalogFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using MvcMusicStore.Application;
 
 namespace MusicStore.Acceptance.Fixtures
@@ -7,17 +8,29 @@ namespace MusicStore.Acceptance.Fixtures
     {
         readonly CatalogFacade CatalogFacade;
 
+        public int AlbumId;
+        public string GenreName;
+
         public CatalogFixture()
         {
             CatalogFacade = new CatalogFacade(new StoreRepository());
         }
 
-        public string GenreName;
-
-        public string GenreAlbums()
+        public string AllAlbumTitles()
         {
             var genreVm = CatalogFacade.FindGenreByName(GenreName);
-            return string.Join(", ", genreVm.Albums.Select(x => x.Title));
+            return string.Join(Environment.NewLine, genreVm.Albums.OrderBy(x => x.Title).Select(x => x.Title));
+        }
+
+        public string GenresToAlbumLinksAreConsistent()
+        {
+            var genreVm = CatalogFacade.FindGenreByName(GenreName);
+            foreach (var album in genreVm.Albums)
+            {
+                var albumVm = CatalogFacade.GetAlbumById(album.AlbumId);
+                if (albumVm.GenreId != genreVm.GenreId) return "NO";
+            }
+            return "YES";
         }
     }
 }
